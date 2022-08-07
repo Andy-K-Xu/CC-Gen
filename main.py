48c9595a3b71c2752961c5691c59adba37f6bda9
Andy-K-Xu <107225222+Andy-K-Xu@users.noreply.github.com>
2022-08-06 20:38:06 -0400

    string of a cc able to be generated

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import time
 import requests
 
 list = ["Visa" , "Mastercard" , "Amex" , "Discover"]
-visa_list = [4539, 4556, 4916, 4532, 4929, 4024, 4485, 4716, 4207, 4266]
-
 company = input('enter:\n')
     
 def visa():
-        beginning = random.choice(visa_list)
-        complete = beginning + random.randrange(0001,9999)
-        print(complete)
+        beginning = random.randrange(4000,4999)
+        repeat = random.randrange(1000,9999)
+        print(str(beginning) + " " + str(repeat) + " " + str(repeat) + " " + str(repeat))
 
 if company == "Visa" in list:
     visa()
