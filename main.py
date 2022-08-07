8077c04c205bbde7fcc27a4348f9fe4098596ed9
Andy-K-Xu <107225222+Andy-K-Xu@users.noreply.github.com>
2022-08-07 14:25:18 -0400

    3 random numbers in list

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import requests
 
 list = ["Visa" , "Mastercard" , "Amex" , "Discover"]
 company = input('enter:\n')
-    
+numbers = []
+
+while len(numbers) < 3:
+        repeat = random.randrange(4000,4999)
+        numbers.append(repeat)
+        
 def visa():
         beginning = random.randrange(4000,4999)
-        repeat = random.randrange(1000,9999)
-        print(str(beginning) + " " + str(repeat) + " " + str(repeat) + " " + str(repeat))
 
+
+        
 if company == "Visa" in list:
     visa()
