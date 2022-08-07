abfa9dfa2d982bd64330708d70653cee945fbdd3
Andy-K-Xu <107225222+Andy-K-Xu@users.noreply.github.com>
2022-08-07 14:31:56 -0400

    successful combination

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ list = ["Visa" , "Mastercard" , "Amex" , "Discover"]
 company = input('enter:\n')
 numbers = []
 
-while len(numbers) < 3:
+    
+while len(numbers) < 4:
         repeat = random.randrange(4000,4999)
         numbers.append(repeat)
         
 def visa():
         beginning = random.randrange(4000,4999)
-
+        listToString = ' '.join([str(i) for i in numbers])
+        print(listToString)
 
         
 if company == "Visa" in list:
