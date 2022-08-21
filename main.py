b65cab498802823b00685fd52f759e5297e9af4f
Andy-K-Xu <107225222+Andy-K-Xu@users.noreply.github.com>
2022-08-21 16:01:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import random
 
+file = open(r"Cards.txt", "w")
+
 def main():
     numbers = []
 
     list = ["Visa", "Mastercard", "Enroute", "Discover", "JCB15", "JCB16", "Voyager"]
-    firstNames = ["Bob", "Greg", "Matt", "Drake", "Odie", "Chip", "Fay", "Tilly", "Amy", "Emily", "Lalia", "Ronat", "Ivria", "Elise", "Bel", "Vereena", "Nana", "Meta", "Thabiti", "Jie", "Rollo", "Cort", "Akanni", "Rothwell", "Emillia", "Pero"]
-    lastNames = ["Smith", "Morva", "Svay", "Capellan", "Hunter", "Huttman", "Eheler", "Davidson", "Rook", "Sheen", "Vacio", "Kingswood", "Mcfolley", "Degnan", "Saric", "Wickert", "Merlini", "Delorme", "Xu", "Jr", "Warwick", "Crill", "Wicked", "Lahue", "Swets", "Santos"]
+    firstNames = ["Bob", "Greg", "Matt", "Drake", "Odie", "Chip", "Fay", "Tilly", "Amy", "Emily", "Lalia", "Ronat",
+                  "Ivria", "Elise", "Bel", "Vereena", "Nana", "Meta", "Thabiti", "Jie", "Rollo", "Cort", "Akanni",
+                  "Rothwell", "Emillia", "Pero"]
+    lastNames = ["Smith", "Morva", "Svay", "Capellan", "Hunter", "Huttman", "Eheler", "Davidson", "Rook", "Sheen",
+                 "Vacio", "Kingswood", "Mcfolley", "Degnan", "Saric", "Wickert", "Merlini", "Delorme", "Xu", "Jr",
+                 "Warwick", "Crill", "Wicked", "Lahue", "Swets", "Santos"]
     company = input("What do you want to generate? \nVisa, Mastercard, Enroute, Discover, JCB15, JCB16, or Voyager?\n")
-
-
 
     def fullName():
         nameOne = random.choice(firstNames)
@@ -23,7 +27,7 @@ def main():
         return date
 
     def randomCVV():
-        cvv = str(random.randrange(100,999))
+        cvv = str(random.randrange(100, 999))
         return cvv
 
     def lastThree():
@@ -33,50 +37,54 @@ def main():
             listToString = ' '.join([str(i) for i in numbers])
         return listToString
 
-
     def visa():
         beginning = str(random.randrange(4000, 4999))
-        end = "Name: " + fullName() + ", " + "Credit Card Number: " + beginning + " " + lastThree() + ", " + "Date: " + randomDate() + ", " + "CVV: " + randomCVV()
+        end = fullName() + " | " + beginning + " " + lastThree() + " | " + randomDate() + " | " + randomCVV()
         print(end)
+        file.write(end + "\n")
         return end
-
 
     def mastercard():
         beginning = str(random.randrange(5100, 5599))
-        end = "Name: " + fullName() + ", " + "Credit Card Number: " + beginning + " " + lastThree() + ", " + "Date: " + randomDate() + ", " + "CVV: " + randomCVV()
+        end = fullName() + " | " + beginning + " " + lastThree() + " | " + randomDate() + " | " + randomCVV()
         print(end)
+        file.write(end + "\n")
         return end
 
     def enroute():
         beginning = str(random.choice([2014, 2149]))
-        end = "Name: " + fullName() + ", " + "Credit Card Number: " + beginning + " " + lastThree() + ", " + "Date: " + randomDate() + ", " + "CVV: " + randomCVV()
+        end = fullName() + " | " + beginning + " " + lastThree() + " | " + randomDate() + " | " + randomCVV()
         print(end)
+        file.write(end + "\n")
         return end
 
     def discover():
         beginning = str(6011)
-        end = "Name: " + fullName() + ", " + "Credit Card Number: " + beginning + " " + lastThree() + ", " + "Date: " + randomDate() + ", " + "CVV: " + randomCVV()
+        end = fullName() + " | " + beginning + " " + lastThree() + " | " + randomDate() + " | " + randomCVV()
         print(end)
+        file.write(end + "\n")
         return end
 
     def jcb15():
         beginning = str(random.choice([1800, 2100]))
-        end = "Name: " + fullName() + ", " + "Credit Card Number: " + beginning + " " + lastThree() + ", " + "Date: " + randomDate() + ", " + "CVV: " + randomCVV()
+        end = fullName() + " | " + beginning + " " + lastThree() + " | " + randomDate() + " | " + randomCVV()
         print(end)
+        file.write(end + "\n")
         return end
 
     def jcb16():
         beginning = str(random.choice([3088, 3096, 3112, 3158, 3337, 3528]))
-        end = "Name: " + fullName() + ", " + "Credit Card Number: " + beginning + " " + lastThree() + ", " + "Date: " + randomDate() + ", " + "CVV: " + randomCVV()
+        end = fullName() + " | " + beginning + " " + lastThree() + " | " + randomDate() + " | " + randomCVV()
         print(end)
+        file.write(end + "\n")
         return end
 
     def voyager():
         beginning = str(8699)
-        end = "Name: " + fullName() + ", " + "Credit Card Number: " + beginning + " " + lastThree() + ", " + "Date: " + randomDate() + ", " + "CVV: " + randomCVV()
+        end = fullName() + " | " + beginning + " " + lastThree() + " | " + randomDate() + " | " + randomCVV()
         print(end)
+        file.write(end + "\n")
         return end
-
 
     if company == "Visa" in list:
         visa()
@@ -95,14 +103,20 @@ def main():
     else:
         main()
 
+
 def ask():
     repeat = input("Do you want to generate another card?\nYes or No?\n")
     if repeat == "Yes":
+        with open("cards.txt", "w") as output:
+            output.write("end")
         main()
         ask()
     else:
+        file.close()
         exit()
 
-#==start==#
+
+
+# ==start==#
 main()
 ask()
